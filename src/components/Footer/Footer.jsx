81267de04595c2ea5ff4b105b2a48ba72272f7c7
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-04-12 21:28:03 +0530

    solve favicon and 404 problem

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Footer.css';
-import {FaFacebookF, FaTwitter} from 'react-icons/fa';
+import {FaFacebookF, FaTwitter, FaLinkedinIn} from 'react-icons/fa';
 import { Link } from "react-router-dom";
 
 const Footer= ()=>{
@@ -8,11 +8,12 @@ const Footer= ()=>{
         <div className="footer">
             <div className="footer-contents">
                 <div className="media-link">
-                    <h1><a href="https://twitter.com/makemytrip/" target={'_blank'}><FaTwitter /></a></h1>
-                    <h1><a href="https://www.facebook.com/makemytrip/" target={'_blank'}><FaFacebookF /></a></h1>
+                    <h1><a href="https://www.linkedin.com/in/dipayanmaji/" target={'_blank'}><FaLinkedinIn /></a></h1>
+                    <h1><a href="https://twitter.com/dipayanmaji11" target={'_blank'}><FaTwitter /></a></h1>
+                    <h1><a href="https://www.facebook.com/dip.ayan.716/" target={'_blank'}><FaFacebookF /></a></h1>
                 </div>
                 <div className="copyright">
-                    <h3>© 2023 <Link to={'/'}>MAKEMYTRIP CLONE</Link></h3> by Dipayan
+                    <h3>© 2023 <Link to={'/'}>MAKEMYTRIP CLONE</Link></h3> by <a href="https://www.linkedin.com/in/dipayanmaji/" target={'_blank'}>Dipayan</a>
                     <br/>
                     Country <h3><Link to={'/india'}>India</Link> USA UAE</h3>
                 </div>
