5e8d987cc6a6ea70247ac871ceb54ac710fa2473
Dipayan Maji <121128467+dipayanmaji@users.noreply.github.com>
2023-05-27 20:27:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Header2 from "./components/Header2/Header2";
-
 
 ReactDOM.render(
     <BrowserRouter>
